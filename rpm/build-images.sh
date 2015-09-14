c163a3fa023ec0eed1e41aa58823142ebe15b4f9
joda70 <elisabetta.ronchieri@cnaf.infn.it>
2015-09-14 16:15:04 +0200

    removed unnecessary condition

--- rpm/build-images.sh
+++ rpm/build-images.sh
@@ -3,9 +3,5 @@ set -ex
 tags="centos5 centos6 centos7"
 
 for t in ${tags}; do
-  STATE=$(echo ${BUILD_PLATFORM} | grep ${t} | awk '{print $1}')
-  if [ ! -z "$STATE" ]
-  then
-     docker build -t italiangrid/pkg.storm-webdav:${t} -f Dockerfile-${t} .
-  fi
+  docker build -t italiangrid/pkg.storm-webdav:${t} -f Dockerfile-${t} .
 done
--- rpm/push-images.sh
+++ rpm/push-images.sh
@@ -8,10 +8,6 @@ if [ -z "${DOCKER_REGISTRY_HOST}" ]; then
 fi
 
 for t in ${tags}; do
-  STATE=$(echo ${BUILD_PLATFORM} | grep ${t} | awk '{print $1}')
-  if [ ! -z "$STATE" ]
-  then
-    docker tag -f  italiangrid/pkg.storm-webdav-server:${t} ${DOCKER_REGISTRY_HOST}/italiangrid/pkg.storm-webdav:${t}
-    docker push ${DOCKER_REGISTRY_HOST}/italiangrid/pkg.storm-webdav:${t}
-  fi
+  docker tag -f  italiangrid/pkg.storm-webdav-server:${t} ${DOCKER_REGISTRY_HOST}/italiangrid/pkg.storm-webdav:${t}
+  docker push ${DOCKER_REGISTRY_HOST}/italiangrid/pkg.storm-webdav:${t}
 done
